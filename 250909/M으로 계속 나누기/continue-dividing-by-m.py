297ed6e9364bd5b048b210895714139436016907
JamesAn13 <162300767+JamesAn13@users.noreply.github.com>
2025-09-09 19:42:33 +0900

    Title: M으로 계속 나누기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250909/M으로 계속 나누기/continue-dividing-by-m.py
+++ 250909/M으로 계속 나누기/continue-dividing-by-m.py
@@ -1,6 +1,9 @@
 N, M = map(int, input().split())
-print(N)
-while N > 0 and N//M != 0:
-    print(N//M)
+# print(N)
+# while N > 0 and N//M != 0:
+#     print(N//M)
+#     N=N//M
+while N > 0:
+    print(N)
     N=N//M
 
