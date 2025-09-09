95f5b4e8bc12bde6a6f5e6e76474c865458a3880
JamesAn13 <162300767+JamesAn13@users.noreply.github.com>
2025-09-09 10:36:45 +0900

    Title: 특정 조건 두 정수 비교, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250909/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
+++ 250909/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -1,10 +1,14 @@
-A,B = map(int, input().split())
-if A<B:
-    print(1, end=" ")
-else:
-    print(0, end=" ")
-if A==B:
-    print(1)
-else:
-    print(0)
+a, b = map(int, input().split())
+a1 , a2 = 0 , 0
 
+if a < b:
+    a1 = 1
+else:
+    a1 = 0
+
+if a == b:
+    a2 = 1
+else:
+    a2 = 0
+
+print(a1, a2)
