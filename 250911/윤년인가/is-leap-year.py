f8ad2b0f2cdc1139caeb229cb565311649adef6f
JamesAn13 <162300767+JamesAn13@users.noreply.github.com>
2025-09-11 22:43:34 +0900

    Title: 윤년인가, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250911/윤년인가/is-leap-year.py
+++ 250911/윤년인가/is-leap-year.py
@@ -1,17 +1,12 @@
 y = int(input())
-if (y % 400 == 0):
-    print('true')
-elif (y % 100 == 0):
-    print('false')
-elif (y % 4 == 0):
-    print('true')
+
+if y % 4 == 0:
+    if y % 100 == 0:
+        if y % 400 == 0:
+            print('true')
+        else:
+            print('false')
+    else:
+        print('true')
 else:
     print('false')
-
-
-# if (y%4 == 0):
-#     print('true')
-# elif (y%100) and ((y%400) != 0):
-#     print('false')
-# else:
-#     print('false')
