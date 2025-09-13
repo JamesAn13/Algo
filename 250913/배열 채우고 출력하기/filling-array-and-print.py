e6e909d0afdd856c469695d26471216ccb1cbaab
JamesAn13 <162300767+JamesAn13@users.noreply.github.com>
2025-09-13 12:05:41 +0900

    Title: 배열 채우고 출력하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250913/배열 채우고 출력하기/filling-array-and-print.py
+++ 250913/배열 채우고 출력하기/filling-array-and-print.py
@@ -1,7 +1,9 @@
 string = list(map(str, input().split()))
 string.reverse()
-for i in range (len(string)):
-    print(string[i], end="")
+for x in string:
+    print(x, end='')
+# for i in range (len(string)):
+#     print(string[i], end="")
 
 # fruits = ["사과", "바나나", "체리", "바나나"]
 # print(fruits.index("바나나"))  # 출력: 1
