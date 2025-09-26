0856cc43a073e385f834329d99ee586f1c38a73d
JamesAn13 <162300767+JamesAn13@users.noreply.github.com>
2025-09-26 10:44:29 +0900

    Title: 약수가 세 개인 수, Time: 54ms, Memory: 17MB, Status: Passed - Codetree

--- 250926/약수가 세 개인 수/numbers-with-three-divisors.py
+++ 250926/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -10,3 +10,14 @@ for i in range(start, end+1):
 print(tcnt)
 
 
+# for i in range(start, end+1):
+#     scnt = 0
+#     tcnt = 0
+#     for j in range(1,i+1):
+#         if i % j == 0:
+#             scnt+=1
+#     if  scnt == 3:
+#         tcnt +=1
+#     print(tcnt)
+
+
