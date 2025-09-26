63bae318bba27c65c174a4f5a754b85c02e73355
JamesAn13 <162300767+JamesAn13@users.noreply.github.com>
2025-09-26 15:09:14 +0900

    Title: 두 배열의 곱, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250926/두 배열의 곱/multiple-of-two-arrays.py
+++ 250926/두 배열의 곱/multiple-of-two-arrays.py
@@ -1,21 +1,42 @@
+# arr1 = [
+#     list(map(int, input().split()))
+#     for _ in range(3)
+# ]
+# input()
+# arr2 = [
+#     list(map(int, input().split()))
+#     for _ in range(3)
+# ]
+# arr3=[]
+
+# for i in range(3):
+#     rowlist = []
+#     for j in range(3):
+#         result = arr1[i][j] * arr2[i][j]
+#         rowlist.append(result)
+#     arr3.append(rowlist)
+
+# for row in arr3:
+#     for elem in row:
+#         print(elem, end = ' ')
+#     print()
 arr1 = [
     list(map(int, input().split()))
     for _ in range(3)
 ]
+input()
 arr2 = [
     list(map(int, input().split()))
     for _ in range(3)
 ]
-arr3=[]
 
-for i in range(3):
-    rowlist = []
-    for j in range(3):
-        result = arr1[i][j] * arr2[i][j]
-        rowlist.append(result)
-    arr3.append(rowlist)
+# 💡 한 줄로 두 배열의 요소별 곱셈을 수행합니다.
+arr3 = [
+    [arr1[i][j] * arr2[i][j] for j in range(3)] 
+    for i in range(3)
+]
 
 for row in arr3:
     for elem in row:
-        print(elem, end = ' ')
+        print(elem, end=' ')
     print()
